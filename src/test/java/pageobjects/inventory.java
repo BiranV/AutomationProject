c23a7fbe76c82204ecfa44917ae67594ccaef483
biran varon <biraniv@gmail.com>
2021-02-08 18:41:49 +0200

    update

--- src/test/java/pageobjects/inventory.java
+++ src/test/java/pageobjects/inventory.java
@@ -1,7 +1,6 @@
 package pageobjects;
 
 import java.util.List;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -50,7 +49,7 @@ public class inventory extends base_page {
 			}
 		}
 	}
-
+	
 	public void nameOfItem() {
 		click(nameOfItem);
 	}
